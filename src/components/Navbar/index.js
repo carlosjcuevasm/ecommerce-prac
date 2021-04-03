9a06a84ae0682d7a770e4a386e27159d69a83bef
srcjcm <48743392+srcjcm@users.noreply.github.com>
2021-04-02 23:16:43 -0400

    firebase auth demo, sign in already created user

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,10 @@
-import { useState, useRef } from 'react';
+//React
+import { useState, useRef, useEffect } from 'react';
+
+//Web server
+import firebase from "firebase/app";
+import "firebase/auth";
+import * as firebaseui from 'firebaseui';
 
 //Styles
 import './styles.scss'
@@ -6,8 +12,38 @@ import './styles.scss'
 //Components
 import Dropdown from '../Dropdown';
 import Modal from '../Navbar';
+import { getDefaultNormalizer } from '@testing-library/dom';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBh1lzK38eeZb-KZ25NrPIF35kYklmiKu4",
+    authDomain: "ecommerce-prac-7fe32.firebaseapp.com",
+    projectId: "ecommerce-prac-7fe32",
+    storageBucket: "ecommerce-prac-7fe32.appspot.com",
+    messagingSenderId: "938524694490",
+    appId: "1:938524694490:web:9504b16aee753294edd2e3"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().signInWithEmailAndPassword('prueba@gmail.com', 'prueba123')
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    alert('entro');
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
 
 function Navbar(props){
+
+   
+
+
 
     //states
     const [isDisabled,setDisabled] = useState('false')
@@ -31,14 +67,15 @@ function Navbar(props){
 
     function modalHandler(){
         setDisabled(!isDisabled);
+        
     }
-    
 
     return (
         <div>
-            <div  className={'modal ' + (isDisabled ? '--disabled': null) }>
-                
+            <div id='firebaseui-auth-container' className={'modal ' + (isDisabled ? '--disabled': null) }>
+            hola
             </div>
+            <div id="loader">Loading...</div>
             <nav className="navbar" style={{position: props.position}}>
             <a href="#" className="navbar__logo"> <h1 className="navbar__logoContent">BizLand<span className="navbar__logoContentSpan">.</span></h1></a>
             <ul className="navbar__menu" ref={navbarMenu}>
